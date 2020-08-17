1a8b55856e8d6bac6ac15691dc47563df1cd08d3
Fischer DeKam <fishfoo@yahoo.com>
2020-08-16 23:04:57 -0600

    Fixes #2 and Fixes #3

--- js/List.js
+++ js/List.js
@@ -6,26 +6,26 @@ function appStart(type) {
 }
 
 function addRow() {
-  var listItem = document.getElementById("listItem");
-  var table = document.getElementById("display-table");
+  var trashCan = '<svg xmlns="http://www.w3.org/2000/svg" onClick="deleteRow(this)" class="icon icon-tabler icon-tabler-trash" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F44336" fill="none" stroke-linecap="round" stroke-linejoin="round" style="float: right;"><path stroke="none" d="M0 0h24v24H0z"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>';
+  var listItem = document.getElementById('listItem');
+  var table = document.getElementById('display-table');
   var rowCount = table.rows.length;
   var row = table.insertRow(rowCount);
   row.insertCell(0).innerHTML= listItem.value;
-  row.insertCell(1).innerHTML= 
-  '<svg xmlns="http://www.w3.org/2000/svg" onClick="deleteRow(this)" class="icon icon-tabler icon-tabler-trash" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F44336" fill="none" stroke-linecap="round" stroke-linejoin="round" style="float: right;"><path stroke="none" d="M0 0h24v24H0z"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>';
+  row.insertCell(1).innerHTML= trashCan;
   document.getElementById("listItem").value="";
 }
  
 function deleteRow(obj) {  
   var index = obj.parentNode.parentNode.rowIndex;
-  var table = document.getElementById("display-table");
+  var table = document.getElementById('display-table');
   table.deleteRow(index);
 }
 
 function save() {
   var r = confirm("Overwrite save data?");
   if (r == true) {
-    var listToSave = document.getElementById("display-table").innerHTML;
+    var listToSave = document.getElementById('display-table').innerHTML;
     var d = new Date();
     listLastSaveDate = d.toLocaleString();
     switch(listType) {
@@ -45,12 +45,8 @@ function save() {
         console.log('save(): Type Error: ', listType);
         break;
     }
-    // sets the date on screen
     document.getElementById('listLastSaveDate').innerHTML = listLastSaveDate;
-    // snackbar DIV
-    var x = document.getElementById("saveSnackbar");
-    x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 2000);
+    displaySaveSnackbar();
   }
 }
 
@@ -59,35 +55,35 @@ function load() {
     case 'Grocery':
       var loadSavedList = localStorage.getItem('SavedGroceryList');
       if (loadSavedList) {
-        document.getElementById("display-table").innerHTML = loadSavedList;
+        document.getElementById('display-table').innerHTML = loadSavedList;
       }
       var savedLastSaveDate = localStorage.getItem('GroceryListLastSaveDate');
       if (savedLastSaveDate) {
         document.getElementById('listLastSaveDate').innerHTML = savedLastSaveDate;
+        displayLoadSnackbar();
       }
-      displayLoadSnackbar();
       break;
     case 'Chores':
       var loadSavedList = localStorage.getItem('SavedChoresList');
       if (loadSavedList) {
-        document.getElementById("display-table").innerHTML = loadSavedList;
+        document.getElementById('display-table').innerHTML = loadSavedList;
       }
       var savedLastSaveDate = localStorage.getItem('ChoresListLastSaveDate');
       if (savedLastSaveDate) {
         document.getElementById('listLastSaveDate').innerHTML = savedLastSaveDate;
+        displayLoadSnackbar();
       }
-      displayLoadSnackbar();
       break;
     case 'ToDo':
       var loadSavedList = localStorage.getItem('SavedToDoList');
       if (loadSavedList) {
-        document.getElementById("display-table").innerHTML = loadSavedList;
+        document.getElementById('display-table').innerHTML = loadSavedList;
       }
       var savedLastSaveDate = localStorage.getItem('ToDoListLastSaveDate');
       if (savedLastSaveDate) {
         document.getElementById('listLastSaveDate').innerHTML = savedLastSaveDate;
+        displayLoadSnackbar();
       }
-      displayLoadSnackbar();
       break;
     default:
       console.log('load(): Type Error: ', listType);
@@ -96,16 +92,67 @@ function load() {
 }
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById('mySidenav').style.width = "100%";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById('mySidenav').style.width = "0";
 }
 
 function displayLoadSnackbar() {
-  // snackbar DIV
-  var x = document.getElementById("loadDataSnackbar");
+  var x = document.getElementById('loadDataSnackbar');
   x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 2000);
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
+}
+
+function displaySaveSnackbar() {
+  var x = document.getElementById('saveSnackbar');
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
+}
+
+function displayDeleteSnackbar() {
+  var x = document.getElementById('deleteSnackbar');
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1000);
+}
+
+function deleteList() {
+  var r = confirm("Confirm delete list?");
+  if (r == true) {
+    var listToDelete = document.getElementById('display-table').innerHTML;
+    var dateToDelete = document.getElementById('listLastSaveDate').innerHTML;
+    switch(listType) {
+      case 'Grocery':
+        listToDelete = '';
+        dateToDelete = '';
+        document.getElementById('display-table').innerHTML = listToDelete;
+        document.getElementById('listLastSaveDate').innerHTML = dateToDelete;
+        localStorage.setItem('SavedGroceryList', listToDelete);
+        localStorage.setItem('GroceryListLastSaveDate', dateToDelete);
+        displayDeleteSnackbar();
+        break;
+      case 'Chores':
+        listToDelete = '';
+        dateToDelete = '';
+        document.getElementById('display-table').innerHTML = listToDelete;
+        document.getElementById('listLastSaveDate').innerHTML = dateToDelete;
+        localStorage.setItem('SavedChoresList', listToDelete);
+        localStorage.setItem('ChoresListLastSaveDate', dateToDelete);
+        displayDeleteSnackbar();
+        break;
+      case 'ToDo':
+        listToDelete = '';
+        dateToDelete = '';
+        document.getElementById('display-table').innerHTML = listToDelete;
+        document.getElementById('listLastSaveDate').innerHTML = dateToDelete;
+        localStorage.setItem('SavedToDoList', listToDelete);
+        localStorage.setItem('ToDoListLastSaveDate', dateToDelete);
+        displayDeleteSnackbar();
+        break;
+      default:
+        console.log('load(): Type Error: ', listType);
+      break;
+    }
+  }
 }
