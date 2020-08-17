2fd609d25e6a789a20d77352a8391c5f06855c17
Fischer DeKam <fishfoo@yahoo.com>
2020-08-16 23:49:10 -0600

    Fixes #6 and prevents adding blank items to list

--- js/List.js
+++ js/List.js
@@ -9,11 +9,13 @@ function addRow() {
   var trashCan = '<svg xmlns="http://www.w3.org/2000/svg" onClick="deleteRow(this)" class="icon icon-tabler icon-tabler-trash" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F44336" fill="none" stroke-linecap="round" stroke-linejoin="round" style="float: right;"><path stroke="none" d="M0 0h24v24H0z"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>';
   var listItem = document.getElementById('listItem');
   var table = document.getElementById('display-table');
-  var rowCount = table.rows.length;
-  var row = table.insertRow(rowCount);
-  row.insertCell(0).innerHTML= listItem.value;
-  row.insertCell(1).innerHTML= trashCan;
-  document.getElementById("listItem").value="";
+  if (listItem.value !== "") {
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+    row.insertCell(0).innerHTML= listItem.value;
+    row.insertCell(1).innerHTML= trashCan;
+    document.getElementById("listItem").value="";
+  }
 }
  
 function deleteRow(obj) {  
