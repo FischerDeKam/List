f5fedb72f2b86abb2ba10cdf88280678f43f6846
Fischer DeKam <fishfoo@yahoo.com>
2020-09-07 16:21:07 -0600

    Fixed #7

--- js/List.js
+++ js/List.js
@@ -1,20 +1,38 @@
 var listType = '';
+var subItemIndex = 0;
 
 function appStart(type) {
   listType = type;
   load();
 }
 
-function addRow() {
+function addRow(rowNumber) {
   var trashCan = '<svg xmlns="http://www.w3.org/2000/svg" onClick="deleteRow(this)" class="icon icon-tabler icon-tabler-trash trash-icon" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F44336" fill="none" stroke-linecap="round" stroke-linejoin="round" style="float: right;"><path stroke="none" d="M0 0h24v24H0z"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>';
+  var arrowDownIcon = '<svg xmlns="http://www.w3.org/2000/svg" onclick="openSubitemNav(this)" class="icon icon-tabler icon-tabler-caret-down" width="52" height="52" viewBox="0 0 24 24" stroke-width="1.5" stroke="#4CAF50" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z"/><path d="M18 15l-6-6l-6 6h12" transform="rotate(180 12 12)" /></svg>';
   var listItem = document.getElementById('listItem');
+  var listSubitem = document.getElementById('listSubitem');
   var table = document.getElementById('display-table');
-  if (listItem.value !== "") {
-    var rowCount = table.rows.length;
-    var row = table.insertRow(rowCount);
-    row.insertCell(0).innerHTML = listItem.value;
-    row.insertCell(1).innerHTML = trashCan;
-    document.getElementById("listItem").value = "";
+  var rowCount = table.rows.length;
+  if (listItem.value !== "" || listSubitem.value !== "") {
+    if (rowNumber >= 0) {
+      var row = table.insertRow(rowNumber);
+      row.insertCell(0).innerHTML = listSubitem.value;
+      row.insertCell(1).innerHTML = arrowDownIcon + trashCan;
+      document.getElementById("listSubitem").value = "";
+    } else {
+      var row = table.insertRow(rowCount);
+      row.insertCell(0).innerHTML = listItem.value;
+      row.insertCell(1).innerHTML = arrowDownIcon + trashCan;
+      document.getElementById("listItem").value = "";
+    }
+  }
+}
+
+function addSubitem() {
+  var listSubitem = document.getElementById('listSubitem');
+  if (listSubitem.value !== "") {
+    addRow(subItemIndex+1);
+    closeSubitemNav();
   }
 }
  
@@ -99,6 +117,15 @@ function openNav() {
 
 function closeNav() {
   document.getElementById('mySidenav').style.width = "0";
+}
+
+function openSubitemNav(obj) {
+  subItemIndex = obj.parentNode.parentNode.rowIndex;
+  document.getElementById('subitemSidenav').style.width = "100%";
+}
+
+function closeSubitemNav() {
+  document.getElementById('subitemSidenav').style.width = "0";
 }
 
 function displayLoadSnackbar() {
