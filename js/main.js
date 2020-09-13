a056cb5cbdffc843c500dbe38f58dfb2e0f879ab
Fischer DeKam <fishfoo@yahoo.com>
2020-09-12 20:28:02 -0600

    Fixed #15

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openPatchNotesSideNav() {
+  document.getElementById('PatchNotesSidenav').style.width = "100%";
+}
+
+function closePatchNotesSideNav() {
+  document.getElementById('PatchNotesSidenav').style.width = "0";
+}
